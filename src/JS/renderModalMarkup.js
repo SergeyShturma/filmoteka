052f776886d4e911562f0a16dee300b0f4f3e215
SergeyShturma <sergeyshturma@gmail.com>
2023-01-28 19:53:35 +0200

    forbid scroll

--- src/JS/renderModalMarkup.js
+++ src/JS/renderModalMarkup.js
@@ -1,75 +1,88 @@
 const refs = {
-      addTo: document.querySelector(".modal_txt"),
-     closeModalBtn: document.querySelector('[data-modal-close]'),
-      backdrop: document.querySelector('.js-backdrop'),
-    watchedBtn: document.querySelector(".button_card_add"), 
-    list: document.querySelector('.js-cards'),
-}
+  addTo: document.querySelector('.modal_txt'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  backdrop: document.querySelector('.js-backdrop'),
+  watchedBtn: document.querySelector('.button_card_add'),
+  list: document.querySelector('.js-cards'),
+};
 
 refs.closeModalBtn.addEventListener('click', onCloseModal);
-refs.backdrop.addEventListener('click', onBackdropClick); 
+refs.backdrop.addEventListener('click', onBackdropClick);
 // refs.watchedBtn.addEventListener("click", addToLocalStorage)
-refs.list.addEventListener('click', movieClick)
-
+refs.list.addEventListener('click', movieClick);
 
 function movieClick(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-   const selectedFilm = evt.target.closest('.js-card');
-    const filmId = selectedFilm.dataset.id;
-    
-    if (selectedFilm) {
-        onOpenModal();
-        fetchOneMovie(filmId)
-            .then(data => {
-                createMarkupCard(data)             
-            })
-    }
+  const selectedFilm = evt.target.closest('.js-card');
+  const filmId = selectedFilm.dataset.id;
+
+  if (selectedFilm) {
+    onOpenModal();
+    fetchOneMovie(filmId).then(data => {
+      createMarkupCard(data);
+    });
+  }
 }
 
-
 function fetchOneMovie(filmId) {
-    return fetch(`https://api.themoviedb.org/3/movie/${filmId}?api_key=671c14eb1babf71c7ecd9b35ab5716a8`)
-        .then(resp => {
-             if (!resp.ok) {
-                 throw new Error(resp.statusText);
-             }
-            return resp.json()
-        })
-  
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${filmId}?api_key=671c14eb1babf71c7ecd9b35ab5716a8`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
 }
 
 function onOpenModal() {
-    document.addEventListener('keydown', onEscapeClick)
-    document.body.classList.add('show-modal');
-    // createMarkupCard();
+  document.addEventListener('keydown', onEscapeClick);
+  document.body.classList.add('show-modal');
+  // createMarkupCard();
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${window.scrollY}px`;
 }
 
 function onCloseModal() {
-    document.removeEventListener('keydown', onEscapeClick)
-    document.body.classList.remove('show-modal');
+  document.removeEventListener('keydown', onEscapeClick);
+  document.body.classList.remove('show-modal');
+  const scrollY = document.body.style.top;
+  document.body.style.position = '';
+  document.body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
 }
 
 function onBackdropClick(evt) {
-    if (evt.currentTarget === evt.target) {
-        onCloseModal();
-    }
+  if (evt.currentTarget === evt.target) {
+    onCloseModal();
+  }
 }
 
 function onEscapeClick(evt) {
-    if (evt.code === 'Escape') {
-        onCloseModal();
-    }
+  if (evt.code === 'Escape') {
+    onCloseModal();
+  }
 }
 
-
 function createMarkupCard(data) {
-    const arr = [];
-    arr.push(data);
-    const markup = arr.map(({ title, vote_average, vote_count, popularity, original_title, genres, overview, poster_path }) => {
+  const arr = [];
+  arr.push(data);
+  const markup = arr
+    .map(
+      ({
+        title,
+        vote_average,
+        vote_count,
+        popularity,
+        original_title,
+        genres,
+        overview,
+        poster_path,
+      }) => {
         const genreList = genres.map(genre => genre.name);
 
-     return `<img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="phonesize_image_movie" class="image_movie_card">
+        return `<img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="phonesize_image_movie" class="image_movie_card">
                 <div class="movie_card_info">
                     <h1 class="movie_title_card_modal">${title}</h1>
                     <div class="rates_info_card_movie">
@@ -86,7 +99,7 @@ function createMarkupCard(data) {
     <p class="votes_number_card"> <span class="span_votes_number_card">${vote_average}</span>/${vote_count}</p>
                     <p class="votes_number_card">${popularity}</p>
                     <p class="votes_number_card">${original_title}</p>
-                    <p class="votes_number_card">${genreList.slice(0,3)}</p>
+                    <p class="votes_number_card">${genreList.slice(0, 3)}</p>
 </div>
                     
 
@@ -95,8 +108,9 @@ function createMarkupCard(data) {
                     <h2 class="about-film-card-info">About</h2>
 
                  <p class="description-movie-card"> ${overview}</p>  
-                </div>`}).join('');
-    refs.addTo.innerHTML = markup;
+                </div>`;
+      }
+    )
+    .join('');
+  refs.addTo.innerHTML = markup;
 }
-
-
