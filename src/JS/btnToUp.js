c0b204dc034bff166b0c493fb347161ed179de6b
SergeyShturma <sergeyshturma@gmail.com>
2023-01-29 14:52:11 +0200

    fix btn

--- src/JS/btnToUp.js
+++ src/JS/btnToUp.js
@@ -1,7 +1,30 @@
-const btnToUp = document.querySelector('.btnToUp')
+// // document.addEventListener('scroll', () => {
+// //   btnToUp.style.display = 'block';
+// // });
+
+const btnToUp = document.querySelector('.btnToUp');
 btnToUp.style.display = 'none';
 
-document.addEventListener("scroll", () => {
-        btnToUp.style.display = 'block';
-})
+function showArr() {
+  let viewportHeight = document.documentElement.clientHeight;
+  let htmlHeight = document.documentElement.scrollHeight;
+  btnToUp.style.display = 'block';
+  if (pageYOffset < viewportHeight) {
+    btnToUp.classList.add('arrow--show');
+  }
+}
+function scrollTo(e) {
+  window.scroll({
+    left: 0,
+    top: e.offsetTop,
+    behavior: 'smooth',
+  });
+}
 
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', showArr);
+
+btnToUp.addEventListener('click', function () {
+  scrollTo(header);
+});
