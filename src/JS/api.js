41bba1ccf9a3940df538158b2a570703a57d6331
ValeryVVV <110850242+ValeryVVV@users.noreply.github.com>
2023-02-01 15:26:44 +0200

    pagination done

--- src/JS/api.js
+++ src/JS/api.js
@@ -3,7 +3,6 @@ import { spinnerPlay, spinnerStop } from './spinner';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
 const API_KEY = '671c14eb1babf71c7ecd9b35ab5716a8';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500/';
@@ -17,11 +16,12 @@ const refs = {
   clearTextContentInInput: document.querySelector('.js-input'),
 };
 refs.onloadMore.style.display = 'none';
-let pageNum = 1;
+let pageNum = 2;
 
-renderTrendCardMarkup();
+// renderTrendCardMarkup();
 
-export function getTrendingMovies() {
+export function getTrendingMovies(pageNum) {
+  console.log('pageNum ', pageNum);
   return fetch(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}&language=ru
     &page=${pageNum}&include_adult=false`)
     .then(resp => {
@@ -30,7 +30,9 @@ export function getTrendingMovies() {
       }
       return resp.json();
     })
-    .catch(err => console.log(err));
+    .catch(error => {
+      console.log('error :>> ', error);
+    });
 }
 
 async function getGenreNames() {
@@ -42,47 +44,49 @@ async function getGenreNames() {
   return genNames;
 }
 
-async function renderTrendCardMarkup() {
+export async function renderTrendCardMarkup(page) {
   const genres = await getGenreNames();
   spinnerPlay();
-  getTrendingMovies()
-    .then(data => data.results)
-    .then(cards =>
-      cards.map(card => {
-        const {
-          genre_ids,
-          poster_path,
-          title,
-          vote_average,
-          release_date,
-          id,
-        } = card;
-        let movieGenres = [];
-        for (const genre of genres) {
-          if (genre_ids.includes(genre.id)) {
-            movieGenres.push(genre.name);
+  return getTrendingMovies(page)
+    .then(({ total_results, results }) => {
+      const markup = results
+        .map(card => {
+          const {
+            genre_ids,
+            poster_path,
+            title,
+            vote_average,
+            release_date,
+            id,
+          } = card;
+          let movieGenres = [];
+          for (const genre of genres) {
+            if (genre_ids.includes(genre.id)) {
+              movieGenres.push(genre.name);
+            }
+            if (movieGenres.length > 3) {
+              movieGenres = [...movieGenres.slice(0, 2), '...Other'];
+            }
           }
-          if (movieGenres.length > 3) {
-            movieGenres = [...movieGenres.slice(0, 2), '...Other'];
-          }
-        }
 
-        const cardMarkup = `<li class='js-card' data-id="${id}">
+          return `<li class='js-card' data-id="${id}">
      <button type="button" class='js-on-card'>
      <img src="${IMG_BASE_URL}${poster_path}" alt="" class='js-card-img'>
      </button>
      <div class='js-movie-descr'>
      <p class='js-movie-title'>${title.toUpperCase()}</p>
      <div class='js-movie-genres'>
-     <p>${movieGenres.join(", ")} | ${release_date.slice(0, 4)}</p>
+     <p>${movieGenres.join(', ')} | ${release_date.slice(0, 4)}</p>
      <span class='js-movie-reiting'>${String(vote_average).slice(0, 3)}</span>
      </div>
      </div>
      </li>`;
-
-        refs.cardsArea.insertAdjacentHTML('beforeend', cardMarkup);
-      })
-    )
+        })
+        .join('');
+      refs.cardsArea.innerHTML = '';
+      refs.cardsArea.insertAdjacentHTML('beforeend', markup);
+      return total_results;
+    })
     .catch(err => console.log(err))
     .finally(() => {
       spinnerStop();
@@ -117,22 +121,22 @@ async function onSearch(evt) {
   if (!searchQuery) {
     errorMessage.classList.add('header__error-message');
     Notify.warning('Sorry, there is no result. Please try another keyword');
-    
+
     setTimeout(function () {
       errorMessage.classList.remove('header__error-message');
     }, 5000);
     // alert('Please, enter your request');
-   return;
+    return;
   }
   await getSearchMovies().then(data => {
-      if (data.total_results === 0) {
+    if (data.total_results === 0) {
       errorMessage.classList.add('header__error-message');
       Notify.warning('Sorry, there is no result. Please try another keyword');
-       return; 
-      } 
-      Notify.success(`We found for you ${searchQuery} movies`);
-      creatMarkup();
-    })
+      return;
+    }
+    Notify.success(`We found for you ${searchQuery} movies`);
+    creatMarkup();
+  });
   // refs.clearTextContentInInput.value = '';
 }
 
@@ -140,42 +144,43 @@ async function creatMarkup() {
   const genres = await getGenreNames();
   getSearchMovies()
     .then(data => data.results)
-    .then(cards =>
-      cards.map(card => {
-        const {
-          genre_ids,
-          poster_path,
-          title,
-          vote_average,
-          release_date,
-          id,
-        } = card;
-        let movieGenres = [];
-        for (const genre of genres) {
-          if (genre_ids.includes(genre.id)) {
-            movieGenres.push(genre.name);
+    .then(cards => {
+      const cardMarkup = cards
+        .map(card => {
+          const {
+            genre_ids,
+            poster_path,
+            title,
+            vote_average,
+            release_date,
+            id,
+          } = card;
+          let movieGenres = [];
+          for (const genre of genres) {
+            if (genre_ids.includes(genre.id)) {
+              movieGenres.push(genre.name);
+            }
+            if (movieGenres.length > 3) {
+              movieGenres = [...movieGenres.slice(0, 2), '...Other'];
+            }
           }
-          if (movieGenres.length > 3) {
-            movieGenres = [...movieGenres.slice(0, 2), '...Other'];
-          }
-        }
 
-        const cardMarkup = `<li class='js-card' data-id="${id}">
+          return `<li class='js-card' data-id="${id}">
      <button type="button" class='js-on-card'>
      <img src="${IMG_BASE_URL}${poster_path}" alt="" class='js-card-img'>
      </button>
      <div class='js-movie-descr'>
      <p class='js-movie-title'>${title.toUpperCase()}</p>
      <div class='js-movie-genres'>
-     <p>${movieGenres.join(", ")} | ${release_date.slice(0, 4)}</p>
+     <p>${movieGenres.join(', ')} | ${release_date.slice(0, 4)}</p>
      <span class='js-movie-reiting'>${String(vote_average).slice(0, 3)}</span>
      </div>
      </div>
      </li>`;
-
-        refs.cardsArea.insertAdjacentHTML('beforeend', cardMarkup);
-      })
-    )
+        })
+        .join('');
+      refs.cardsArea.insertAdjacentHTML('beforeend', cardMarkup);
+    })
     .catch(err => console.log(err));
 }
 
@@ -209,12 +214,12 @@ getMovieVideos()
   .then(data => data)
   .catch(err => console.log(err));
 
-refs.onloadMore.addEventListener('click', onLoadMoreBtn);
-setTimeout(() => {
-  refs.onloadMore.style.display = 'block';
-}, 2000);
-function onLoadMoreBtn() {
-  pageNum += 1;
-  creatMarkup();
-  renderTrendCardMarkup();
-}
+// refs.onloadMore.addEventListener('click', onLoadMoreBtn);
+// setTimeout(() => {
+//   refs.onloadMore.style.display = 'block';
+// }, 2000);
+// function onLoadMoreBtn() {
+//   pageNum += 1;
+//   creatMarkup();
+//   renderTrendCardMarkup();
+// }
